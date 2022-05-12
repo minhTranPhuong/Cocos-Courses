2b8fee517656e0a510b1f0f4415bcc038faf423c
minhTranPhuong <99860039+minhTranPhuong@users.noreply.github.com>
2022-05-12 18:17:27 +0700

    Add files via upload

--- lab07/assets/script/controller.js
+++ lab07/assets/script/controller.js
@@ -3,81 +3,84 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        isActionMove : false,
-        isActionJump: false,
-        arrKey:null
+        _keys: null,
+        _isAction: false,
+        _isDone: false
     },
 
-    onLoad () {
-        this.arrKey = new Map();
-        // this.input.on(cc.Input.EventType.KEY_DOWN, this.onKeyDown , this)
-        this.onEvent();
+    onKeyDown(e) {
+        if (!this._keys.has(e.keyCode) && this._isDone == true) {
+            this._isAction = false;
+        }
+        this._keys.set(e.keyCode, e);
+
+        if (this._isAction == true) {
+            return;
+        }
+        this._isAction = true;
+        this._isDone = false;
+        this.catchEvent(e.keyCode);
+    },
+
+    catchEvent(keyCode) {
+        switch (keyCode) {
+            case cc.macro.KEY.up:
+                if (this._keys.has(cc.macro.KEY.right)) {
+                    Emitter.instance.emit("jumpRight","right");
+                }
+                else if (this._keys.has(cc.macro.KEY.left)) {
+                    Emitter.instance.emit("jumpRight","left");
+                }
+                else{
+                    Emitter.instance.emit("jump");
+                }
+                break;
+            case cc.macro.KEY.right:
+                Emitter.instance.emit("goRight","right");
+                break;
+            case cc.macro.KEY.space:
+                if (this._keys.has(cc.macro.KEY.right)) {
+                    Emitter.instance.emit("runRight","right");
+                }
+                else if(this._keys.has(cc.macro.KEY.left)){
+                    Emitter.instance.emit("runRight","left");
+                }
+                break;
+            case cc.macro.KEY.left:
+                Emitter.instance.emit("goRight","left");
+                break;
+            case cc.macro.KEY.q:
+                Emitter.instance.emit("shoot");
+            default: break;
+        }
+    },
+
+    onKeyUp(e) {
+        Emitter.instance.emit("keyUp")
+        this._keys.delete(e.keyCode);
+        // for(const value of this._keys.values()){
+        //     this.onKeyDown(value)
+        //     return;
+        // }
+    },
+    onLoad() {
+        this._keys = new Map();
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN,
+            this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP,
+            this.onKeyUp, this);
         Emitter.instance.registerEvent("doneEvent", this.dontEvent.bind(this));
     },
 
-    dontEvent(){
-        cc.log(this)
-        this.isActionJump = false;
-        this.isActionMove = false;
+    dontEvent() {
+        this._isAction = false;
+        this._isDone = true;
     },
 
-    onEvent(){
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyJump, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDownMove, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
-    },
-
-    // onKeyUp(evt){
-    //     Emitter.instance.emit("onKeyUp");
-    //     this.arrKey.delete(evt.keyCode)
-    //     for(const [key,value] of this.arrKey){
-    //         cc.log(this.isActionJump,this.isActionMove);
-    //         if(key == 38){
-    //             this.onKeyJump(value);
-    //             return;
-    //         }
-    //         this.onKeyDownMove(value);
-    //     }
-    // },
-
-    onKeyDownMove(evt){
-        cc.log(evt)
-        this.arrKey.set(evt.keyCode , evt)
-        if(this.isActionMove == true){
-            return;
-        }
-        this.isActionMove = true;
-        switch(evt.keyCode){
-            case cc.macro.KEY.left:
-                break;
-            case cc.macro.KEY.right:
-                Emitter.instance.emit("goRight", Emitter);
-                break;
-            default : 
-                this.isActionMove = false;
-                break;
-        }
-    },  
-
-    onKeyJump(evt){
-        if(this.isActionJump == true){
-            return;
-        }
-        this.isActionJump = true;
-        switch(evt.keyCode){
-            case cc.macro.KEY.up: 
-            Emitter.instance.emit("jump", Emitter);
-            break;
-        default :
-            this.isActionJump= false;
-            break;
-        }
-    },
-
-    start () {
+    start() {
 
     },
 
-    update (dt) {
+    update(dt) {
     },
 });
--- lab07/assets/script/sSpinBoy.js
+++ lab07/assets/script/sSpinBoy.js
@@ -3,49 +3,121 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        spinBoy: sp.Skeleton
+        spinBoy: sp.Skeleton,
+        _scaleX : 0,
+        _directed : "right",
+        _action: null,
+        _y:0,
+        _x:0
     },
 
     onLoad () {
+        this._y = this.node.y;
+        this._x = this.node.x;
+        this._scaleX = this.node.scaleX;
         Emitter.instance.registerEvent("jump",this.spinBoyJump.bind(this));
+        Emitter.instance.registerEvent("jumpRight",this.spinBoyJumpRight.bind(this));
+        Emitter.instance.registerEvent("runRight",this.spinBoyRunRight.bind(this));
         Emitter.instance.registerEvent("goRight",this.spinBoyGoRight.bind(this));
-        Emitter.instance.registerEvent("onKeyUp",this.onkeyUp.bind(this));
+        Emitter.instance.registerEvent("keyUp",this.keyUp.bind(this));
+        Emitter.instance.registerEvent("shoot",this.shoot.bind(this));
     },
 
-    onkeyUp(){
-        this.spinBoy.setCompleteListener(()=>{
-            // this.spinBoy.addAnimation(0,"idle",false);
-            this.spinBoy.clearTracks();    
-            this.spinBoy.setToSetupPose();
-            this.spinBoy.addAnimation(0,'idle',true);
-        })
-    },
-
-    spinBoyJump(emitter){
-        this.spinBoy.setAnimation(0,"jump",false);
+    shoot(){
+        this.spinBoy.setAnimation(0,"shoot",false);
+        cc.log(this.spinBoy)
         this.spinBoy.setCompleteListener(()=>{
             Emitter.instance.emit("doneEvent");
-            this.spinBoy.setAnimation(0,"idle",false);
-            this.spinBoy.clearTracks();
-            
-        })
+       })
     },
 
-    spinBoyGoRight(emitter){
-        var moveLeft = cc.moveBy(0.8,cc.v2(200,0));
-        this.node.runAction(moveLeft);
+    keyUp(){
+        //this.spinBoy.setCompleteListener(()=>{
+            this.node.stopAction(this._action)
+            Emitter.instance.emit("doneEvent");
+            this.spinBoy.clearTracks();    
+            this.spinBoy.setToSetupPose();
+            this.spinBoy.setAnimation(0,'idle',true);
+        //})
+    },
+
+    spinBoyJumpRight(act){
+        cc.log(this.node.y)
+        var direction = this.spinBoyTurn(act)
+        cc.tween(this.node)
+            .to(0.2,{position: cc.v2(this.node.x + direction*25, this.node.y + 50)}).call(()=>{
+                cc.log(this.node.x,this.node.y)
+            })
+            .to(0.2,{position: cc.v2(this.node.x + direction*50, this.node.y + 80)})
+            .call(()=>{
+                cc.log(this.node.x,this.node.y)
+            })
+            .to(0.2,{position: cc.v2(this.node.x + direction*100, this.node.y + 50)})
+            .call(()=>{
+                cc.log(this.node.x,this.node.y)
+            })
+            .to(0.2,{position: cc.v2(this.node.x + direction*125, this.node.y)})
+            .call(()=>{
+                cc.log(this.node.x,this.node.y)
+            })
+            .start();
+        this.spinBoy.setAnimation(0,"run",false);
+        this.spinBoy.setAnimation(0,"hoverboard",false);
+        this.spinBoy.setCompleteListener(()=>{
+             Emitter.instance.emit("doneEvent");
+        })
+        cc.log(this.node.y)
+    },
+
+    spinBoyJump(){
+        cc.log(this.node.y)
+        cc.tween(this.node)
+            .to(0.4,{position: cc.v2(this.node.x , this.node.y + 70)})
+            .to(0.4,{position: cc.v2(this.node.x , this._y)})
+            .start();
+        this.spinBoy.setAnimation(0,"hoverboard",false);
+        this.spinBoy.setCompleteListener(()=>{
+            Emitter.instance.emit("doneEvent");
+       })
+    },
+
+
+    spinBoyRunRight(act){
+        var direction = this.spinBoyTurn(act)
+        let action = cc.moveBy(0.8,cc.v2(direction * 100,0));
+        this._action = this.node.runAction(action);
         this.spinBoy.setAnimation(0,"run",false);
         this.spinBoy.setCompleteListener(()=>{
             Emitter.instance.emit("doneEvent");
-            this.spinBoy.setAnimation(0,"idle",false);
-            this.spinBoy.clearTracks();
-            
-        })
+       })
+    },
+
+    spinBoyGoRight(act){
+        var direction = this.spinBoyTurn(act)
+        let action = cc.moveBy(0.9,cc.v2(direction * 50,0));
+        this._action = this.node.runAction(action);
+        this.spinBoy.setAnimation(0,"walk",false);
+        this.spinBoy.setCompleteListener(()=>{
+            Emitter.instance.emit("doneEvent");
+       })
+    },
+
+    spinBoyTurn(act){
+        var direction = 1;
+        if(act == "right"){
+            this.node.scaleX = this._scaleX;
+        }
+        else{
+            this.node.scaleX = -this._scaleX;
+            direction = -1;
+        }
+        return direction;
     },
 
     start () {
 
     },
 
-    // update (dt) {},
+    update (dt) {
+    },
 });
